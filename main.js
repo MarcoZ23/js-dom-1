995c0d72970881d95a579bf46b77a029f43faa22
MarcoZ23 <zollomarco23@gmail.com>
2026-03-15 12:40:08 +0100

    complete task bonus

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const lampadina = document.getElementById('white');
 button.addEventListener('click', 
                          
  function onOff () {
-    white.src = "img/yellow_lamp.png"; 
-    
- }
-                         
-);
+    if (lampadina.src.includes("white_lamp.png")) {
+        lampadina.src = "img/yellow_lamp.png"; }
+    else {
+        lampadina.src = "img/white_lamp.png"};
+}
+)
+
+
 
