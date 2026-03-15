3c1ca7578ab2023586b1c60b86630d50425f5771
MarcoZ23 <zollomarco23@gmail.com>
2026-03-15 12:19:36 +0100

    add funcion onOff

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const button = document.querySelector('.btn');
+const lampadina = document.getElementById('white');
+button.addEventListener('click', 
+                         
+ function onOff () {
+    white.src = "img/yellow_lamp.png"; 
+    
+ }
+                         
+);
 
